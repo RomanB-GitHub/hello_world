5a69f26449baa58b40c446b786872b769bc1c968
Roman Bovbalan <73779111+RomanB-GitHub@users.noreply.github.com>
2020-12-08 14:14:51 +0200

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello GOIT World Here I Am!!!");
+        System.out.println("Java become interesting more and more!!!");
     }
 }
- 
